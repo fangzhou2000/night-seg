ceb4e5cd5c475aa33b8d65be1363746ecfce3bf6
Tian Qijian <1741919942@qq.com>
2023-03-06 19:35:25 +0800

    part of mmseg

---
+++ my-daformer/mmseg/models/decode_heads/segformer_head.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+from mmcv.cnn import ConvModule
+
+from mmseg.ops import resize
+from ..builder import HEADS
+from .decode_head import BaseDecodeHead
+
+
+class MLP(nn.Module):
+    
